e03c5c71685090a5539443825573039b02c320a4
Grégoire Saunier <gregoire.saunier@ekunn.com>
2019-01-08 16:51:42 +0100

    better search of the gcode command response

--- test/hotend_cooling/parse_hotend_cooling_log.py
+++ test/hotend_cooling/parse_hotend_cooling_log.py
@@ -25,8 +25,8 @@ fname = sys.argv[1]
 
 # Test file path
 if not os.path.isfile(fname):
-   print("File %s does not exist. Exiting...") % fname
-   sys.exit()
+    print("File %s does not exist. Exiting...") % fname
+    sys.exit()
 
 
 # format line to help parser
@@ -45,8 +45,9 @@ with open(fname) as f:
 
         # Read m31 to get time and test start
         if 'm31' in line:
-            line = format_line(f.next())                            # read returned value of m31 command
-            minsec = re.findall(r'\d+', line)                       # extract minutes and seconds
+            while not line.startswith("recv: echo:"):               # search for the response
+                line = format_line(f.next())                        # read next line
+                minsec = re.findall(r'\d+', line)                   # extract minutes and seconds
             time_logged.append(int(minsec[0])*60 + int(minsec[1]))  # convert extracted time in second and save it
             #print("time : %s") % (time_logged)
             
@@ -56,12 +57,17 @@ with open(fname) as f:
 
             # Read temperature
             if ("m105" in line) and num_measures > 0:
-                line = format_line(f.next())
-                print("%10s %10s") % (line.split()[2], line.split()[4])
+                while not line.startswith("recv: ok t:"):                # search for the response
+                    line = format_line(f.next())                         # read next line
+                print("%10s %10s") % (line.split()[2], line.split()[4])  # display hotend and bed temperatures
                 num_measures -= 1
 
 
-# Display delta to 50degC
-old_seconds = datetime.timedelta(seconds = time_logged[0])
-new_seconds = datetime.timedelta(seconds = time_logged[len(time_logged)-1])
-print("delta time : %s") % (new_seconds - old_seconds)
+# Display delta time
+if len(time_logged) <= 0:
+    print("File %s does not exist. Exiting...") % fname
+    sys.exit()
+else:
+    old_seconds = datetime.timedelta(seconds = time_logged[0])
+    new_seconds = datetime.timedelta(seconds = time_logged[len(time_logged)-1])
+    print("delta time : %s") % (new_seconds - old_seconds)
