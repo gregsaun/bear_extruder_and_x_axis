812ddd41af5ed954a58fb500b6823f386db1823e
gregsaun <gregoire.saunier@ekunn.com>
2019-06-10 15:49:31 +0200

    update to latest version

--- src/create_printed_parts_zips.sh
+++ src/create_printed_parts_zips.sh
@@ -19,16 +19,16 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Directories and paths
 common_parts_dir="${SCRIPTDIR}/../printed_parts/stl/common_to_all_versions"
-mk25_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk2.5"
-mk3_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk3"
+mk25s_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk2.5s"
+mk3s_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk3s"
 src_dir="${SCRIPTDIR}/"
-dest_dir="${SCRIPTDIR}/zips"
+dest_dir="${SCRIPTDIR}/../printed_parts/"
 mkdir -p "${dest_dir}"
 
 # Zip files
-mk25_zip_file="${dest_dir}/bear_extruder_mk25.zip"
-mk3_zip_file="${dest_dir}/bear_extruder_mk3.zip"
-src_zip_file="${dest_dir}/bear_extruder_sources.zip"
+mk25s_zip_file="${dest_dir}/bear_extruder_x_axis_mk25s.zip"
+mk3s_zip_file="${dest_dir}/bear_extruder_x_axis_mk3s.zip"
+src_zip_file="${dest_dir}/bear_extruder_x_axis_sources.zip"
 
 # Test if zip package is installed
 if [ $(dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
@@ -38,16 +38,16 @@ fi
 
 # Delete previous zip files
 echo "Delete old zip files"
-rm -f $mk25_zip_file
-rm -f $mk3_zip_file
-rm -f $src_zip_file
+rm -f $mk25s_zip_file
+rm -f $mk3s_zip_file
+rm -f $srcs_zip_file
 
 # Zip files
 echo "Zip MK2.5"
-cp -r ${common_parts_dir}/* ${mk25_parts_dir}/* -t ${dest_dir}
-cd $dest_dir && zip -q -r -m $mk25_zip_file ./*.st* && cd - > /dev/null
+cp -r ${common_parts_dir}/* ${mk25s_parts_dir}/* -t ${dest_dir}
+cd $dest_dir && zip -q -r -m $mk25s_zip_file ./*.st* && cd - > /dev/null
 echo "Zip MK3"
-cp -r ${common_parts_dir}/* ${mk3_parts_dir}/* -t ${dest_dir}
-cd $dest_dir && zip -q -r -m $mk3_zip_file ./*.st* && cd - > /dev/null
+cp -r ${common_parts_dir}/* ${mk3s_parts_dir}/* -t ${dest_dir}
+cd $dest_dir && zip -q -r -m $mk3s_zip_file ./*.st* && cd - > /dev/null
 echo "Zip Sources"
 cd $src_dir && zip -q -r $src_zip_file ./* -x "zips/*" && cd - > /dev/null
