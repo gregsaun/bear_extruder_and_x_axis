479b014c8d3a25da562397b301ca6fc03a847f44
gregsaun <gregoire.saunier@ekunn.com>
2019-06-27 12:49:14 +0200

    create function to zip files

--- src/create_printed_parts_zips.sh
+++ src/create_printed_parts_zips.sh
@@ -15,17 +15,19 @@
 #
 ################################################################################
 
+# Sources directories and paths
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Directories and paths
 common_parts_dir="${SCRIPTDIR}/../printed_parts/stl/common_to_all_versions"
 mk25s_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk2.5s"
 mk3s_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk3s"
-src_dir="${SCRIPTDIR}/"
-dest_dir="${SCRIPTDIR}/../printed_parts/"
+src_dir="${SCRIPTDIR}"
+
+# Destination directories and paths
+dest_dir="${SCRIPTDIR}/../printed_parts"
+temp_dir="${SCRIPTDIR}/tmp_zip"
 mkdir -p "${dest_dir}"
 
-# Zip files
+# Destination zip files
 mk25s_zip_file="${dest_dir}/bear_extruder_x_axis_mk25s.zip"
 mk3s_zip_file="${dest_dir}/bear_extruder_x_axis_mk3s.zip"
 src_zip_file="${dest_dir}/bear_extruder_x_axis_sources.zip"
@@ -36,18 +38,39 @@ if [ $(dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -e
     exit -1
 fi
 
-# Delete previous zip files
-echo "Delete old zip files"
-rm -f $mk25s_zip_file
-rm -f $mk3s_zip_file
-rm -f $srcs_zip_file
+# Compress zip files
+# Arguments:
+#   $1 = files to compress (/path/to/files/*)
+#   $2 = path to future zip file (example: path/to/compressedfile.zip)
+#   $3 = optional. file extension to exclude
+function create_zip {
+    source_dir="$1"
+    zip_path="$2"
+
+    # Remove existing files
+    if [ -d "${temp_dir}" ]; then
+        rm $zip_path
+    fi
+    if [ -d "${temp_dir}" ]; then
+        rm -f ${temp_dir}/*
+    else
+        mkdir -p "$temp_dir"
+    fi
+
+    # Copy source and create zip
+    cp $source_dir -t "${temp_dir}/"
+    cd "$temp_dir" && zip -q -r -m "$zip_path" ./*
+
+    # Clean up
+    rm -f ${temp_dir}/* && rm -d ${temp_dir}
+}
 
 # Zip files
 echo "Zip MK2.5"
-cp -r ${common_parts_dir}/* ${mk25s_parts_dir}/* -t ${dest_dir}
-cd $dest_dir && zip -q -r -m $mk25s_zip_file ./*.st* && cd - > /dev/null
+create_zip "${common_parts_dir}/* ${mk25s_parts_dir}/*" "$mk25s_zip_file"
+
 echo "Zip MK3"
-cp -r ${common_parts_dir}/* ${mk3s_parts_dir}/* -t ${dest_dir}
-cd $dest_dir && zip -q -r -m $mk3s_zip_file ./*.st* && cd - > /dev/null
+create_zip "${common_parts_dir}/* ${mk3s_parts_dir}/*" "$mk3s_zip_file"
+
 echo "Zip Sources"
-cd $src_dir && zip -q -r $src_zip_file ./* -x "zips/*" && cd - > /dev/null
+create_zip "${src_dir}/*.f*" "$src_zip_file"
