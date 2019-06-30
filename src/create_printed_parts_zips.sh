48812c2b780dd8034d9427123222effe8b13c5e0
gregsaun <gregoire.saunier@ekunn.com>
2019-06-30 21:15:56 +0200

    add removal of optional parts. Zip creation more robust

--- src/create_printed_parts_zips.sh
+++ src/create_printed_parts_zips.sh
@@ -4,11 +4,12 @@
 #
 # Create Printed Parts Zip file for all printer version
 #
-# Author: Grégoire Saunier
-# Website: www.ekunn.com
+# Author: Gregoire Saunier
+# Website: http://github.com/gregsaun
 #
 # Description:
-# Create ZIP file for printed parts of each printer version. Include sources.
+# Create ZIP file for printed parts of each printer version, optional parts
+# and sources.
 #
 # Resources:
 #   - http://github.com/gregsaun
@@ -20,17 +21,15 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 common_parts_dir="${SCRIPTDIR}/../printed_parts/stl/common_to_all_versions"
 mk25s_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk2.5s"
 mk3s_parts_dir="${SCRIPTDIR}/../printed_parts/stl/mk3s"
+opt_parts_dir="${SCRIPTDIR}/../optional_parts"
 src_dir="${SCRIPTDIR}"
 
-# Destination directories and paths
-dest_dir="${SCRIPTDIR}/../printed_parts"
-temp_dir="${SCRIPTDIR}/tmp_zip"
-mkdir -p "${dest_dir}"
-
 # Destination zip files
-mk25s_zip_file="${dest_dir}/bear_extruder_x_axis_mk25s.zip"
-mk3s_zip_file="${dest_dir}/bear_extruder_x_axis_mk3s.zip"
-src_zip_file="${dest_dir}/bear_extruder_x_axis_sources.zip"
+temp_dir="${SCRIPTDIR}/.tmp_zip"
+mk25s_zip="${SCRIPTDIR}/../printed_parts/bear_extruder_x_axis_mk25s.zip"
+mk3s_zip="${SCRIPTDIR}/../printed_parts/bear_extruder_x_axis_mk3s.zip"
+src_zip="${SCRIPTDIR}/../printed_parts/bear_extruder_x_axis_sources.zip"
+opt_parts_zip="${SCRIPTDIR}/../optional_parts/bear_extruder_x_axis_optional_parts.zip"
 
 # Test if zip package is installed
 if [ $(dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
@@ -46,20 +45,21 @@ function create_zip {
     zip_path="$1"
 
     # Remove existing files
-    if [ -d "$temp_dir" ]; then
-        rm "$zip_path"
-    fi
-    if [ -d "$temp_dir" ]; then
-        rm -f "$temp_dir"/*
+    if [[ -d "$temp_dir" ]]; then
+        rm "$temp_dir"/*
     else
         mkdir -p "$temp_dir"
+    fi
+    if [[ -f "$zip_path" ]]; then
+        rm "$zip_path"
     fi
 
     # Copy source into temp directory
     shift
     while test $# -gt 0; do
+        echo -n "."
         if [[ -d $1 ]]; then      # if directory
-            cp "$1"/* -t "$temp_dir"/
+            cp -r "$1"/. -t "$temp_dir"/
         elif [[ -f $1 ]]; then    # if file
             cp "$1" -t "$temp_dir"/
         else
@@ -70,18 +70,27 @@ function create_zip {
     done
 
     # Compress all files into temp directory
-    cd "$temp_dir" && zip -q -r -m "$zip_path" ./*
+    echo -n "."
+    cd "$temp_dir" && zip -q -r -m -T "$zip_path" ./* || exit 1
 
     # Clean up
-    rm -f "$temp_dir"/* && rm -d "$temp_dir"
+    echo -n "."
+    rm -f "$temp_dir"/*.* && rm -d "$temp_dir"
+    echo ""
 }
 
-# Zip files
+
+######## MAIN ########
+
+# Create zip files
 echo "Zip MK2.5S"
-create_zip "$mk25s_zip_file" "${common_parts_dir}" "${mk25s_parts_dir}"
+create_zip "$mk25s_zip" "$common_parts_dir" "$mk25s_parts_dir"
 
 echo "Zip MK3S"
-create_zip "$mk3s_zip_file" "${common_parts_dir}" "${mk3s_parts_dir}"
+create_zip "$mk3s_zip" "$common_parts_dir" "$mk3s_parts_dir"
 
 echo "Zip Sources"
-create_zip "$src_zip_file" "${src_dir}"/*.f*
+create_zip "$src_zip" "${src_dir}"/*.f*
+
+echo "Optional Parts"
+create_zip "$opt_parts_zip" "$opt_parts_dir"
